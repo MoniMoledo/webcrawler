788ab0098d1734f47888ace588117f0ad32c4c7b
Monique Moledo <moniquemoledo@hotmail.com>
2017-05-29 01:10:28 -0300

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,8 @@
+name := "WebCrawler"
+
+version := "1.0"
+
+scalaVersion := "2.12.2"
+
+libraryDependencies += "com.typesafe.play" %% "play-json" % "2.3.4"
+        
---
+++ src/main/scala/Crawler.java
@@ -0,0 +1,70 @@
+import com.google.gson.JsonElement;
+import com.webhoseio.sdk.WebhoseIOClient;
+
+import java.io.*;
+import java.net.URISyntaxException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.zip.GZIPOutputStream;
+
+public class Crawler {
+
+    public static BufferedWriter createWriter(String fileName) throws IOException {
+
+        SimpleDateFormat sdfDate = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
+        Date now = new Date();
+        String strDate = sdfDate.format(now);
+        fileName += strDate + ".gz";
+        GZIPOutputStream zip = new GZIPOutputStream(
+                new FileOutputStream(new File(fileName)));
+        BufferedWriter bw = new BufferedWriter(
+                new OutputStreamWriter(zip, "UTF-8"));
+        return bw;
+    }
+
+    private static String getQueryString() {
+
+        String OR = " OR ";
+        String AND = " AND ";
+        String location_filter = "location: ";
+
+        String zika = "\"zika\"";
+        String febreAmarela = "\"febre amarela\"";
+        String chikungunya = "\"chikungunya\"";
+        String dengue = "\"dengue\"";
+
+        String filters = "(" + zika + OR + febreAmarela + OR + dengue + OR + chikungunya + ")" + AND + location_filter + "brazil";
+
+        return filters;
+    }
+
+    public static void main(String[] args) throws IOException, URISyntaxException {
+
+        try (BufferedWriter bw = createWriter("webhose")) {
+
+            String api_key = "1e8f7935-bacd-4c4e-a055-f1e52f8bbadd";
+            String filters = getQueryString();
+
+            WebhoseIOClient webhoseClient = WebhoseIOClient.getInstance(api_key);
+            // Create set of queries
+            Map<String, String> queries = new HashMap<String, String>();
+
+            queries.put("q", filters);
+
+            // Fetch query result
+            JsonElement result = webhoseClient.query("filterWebContent", queries);
+
+            int moreResultsAvailable = result.getAsJsonObject().get("moreResultsAvailable").getAsInt();
+
+            while (moreResultsAvailable > 0) {
+
+                bw.write(result.getAsJsonObject().get("posts").getAsString());
+                result = webhoseClient.getNext();
+                moreResultsAvailable = result.getAsJsonObject().get("moreResultsAvailable").getAsInt();
+            }
+
+        }
+    }
+}
